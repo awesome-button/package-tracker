c7218ef968a6e76c3bd6c352392b1d2cb3d04314
awesome-button <oleshko.lena@gmail.com>
2020-11-04 18:41:16 +0100

    set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Homepage from "./components/pages/Homepage";
+import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+
+          <Route path="/parcels/:id"></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
