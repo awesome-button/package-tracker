c5940a7423e10f61aeb6500857d5724265bce8d8
awesome-button <oleshko.lena@gmail.com>
2020-11-04 17:52:54 +0100

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import Homepage from "./components/pages/Homepage";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+};
 
 export default App;
