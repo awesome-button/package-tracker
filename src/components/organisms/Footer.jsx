5dc887b65d1f5d327c4ba7e92d9d32370fc6072c
awesome-button <oleshko.lena@gmail.com>
2020-11-04 17:26:44 +0100

    created basic organism components and basic Homepage structure

---
+++ src/components/organisms/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h1>Here is the Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../organisms/Header";
+import Footer from "../organisms/Footer";
+import Search from "../organisms/Search";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
