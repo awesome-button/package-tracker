8b7af1547177144b3f51f7806eae1e543d65011a
awesome-button <oleshko.lena@gmail.com>
2020-11-04 18:33:25 +0100

    implemented search component with useState hook

--- src/components/organisms/Search.jsx
+++ src/components/organisms/Search.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [query, setQuery] = useState("");
+
+  const searchPackage = (event) => {
+    event.preventDefault();
+    //React Router's history push function
+    console.log(query);
+  };
+
   return (
     <div className="search">
-      <h1>Here is the Search</h1>
+      <h1>Track your package</h1>
+      <p>Are you expecting a delivery? Cool! Fill in your package ID below</p>
+
+      <form type="submit" onSubmit={(event) => searchPackage(event)}>
+        <label>Your package ID:</label>
+        <input
+          placeholder="5625"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type="submit" onClick={(event) => searchPackage(event)}>
+          Track
+        </button>
+      </form>
     </div>
   );
 };
