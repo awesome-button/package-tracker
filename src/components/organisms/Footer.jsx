746070ec673a688bda0ae6349a725df5c103faf2
awesome-button <oleshko.lena@gmail.com>
2020-11-05 14:23:15 +0100

    implemented footer component

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <h1>Here is the Footer</h1>
+      <h2>Customer Service</h2>
+
+      <h3>Call us</h3>
+      <a href="tel: 0794581288">079-458 12 88</a>
+      <h3>Phone hours</h3>
+      <span>Monday-Friday 08-22</span>
+      <span>Saturday-Sunday 10-17</span>
+
+      <h3>Mail us</h3>
+      <a href="mailto:yellow-co@yellow-co.com">yellow-co@yellow-co.com</a>
+
+      <h3>Send us a text</h3>
+      <a href="sms: 0794581200">079-458 12 00</a>
+
+      {/* maybe could add social media links */}
     </footer>
   );
 };
